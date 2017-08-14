2c672841023a318841bb638c40b6e3d954876e0f
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-08-14 16:27:35 +0100

    User Input

--- directory.rb
+++ directory.rb
@@ -28,7 +28,27 @@ students = [
    puts "Overall, we have #{names.count} great students"
  end
 
+ def input_students
+   puts "Please enter the names of the students"
+   puts "To finish, just hit the return key twice"
+   # Empty array
+   students = []
+   # get the furst name
+   name = gets.chomp
+   # while the name is not empty: repeat
+   while !name.empty? do
+     #add the student hash to the array
+     students << {name: name, cohort: :november}
+     puts "Now we have #{students.count} students"
+     # get another name from the user
+     name = gets.chomp
+   end
+   # return the array of students
+     students
+   end
+
 # Calling the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
