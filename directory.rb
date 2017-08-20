ade330ca5d45656d01b27baedce8b6ac272df8a3
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-08-20 23:35:58 +0100

    Cleaning up and pluralisation

--- directory.rb
+++ directory.rb
@@ -2,57 +2,48 @@
 def input_students
    puts "Please enter the names of the students"
    puts "To finish, just hit the return key twice"
-   # Empty array
-   students = []
-   # get the furst name
-   name = gets.chomp.capitalize
-   #cohort
-   puts "Please enter the month of the cohort"
-   cohort = gets.chomp.capitalize
-   # hobby
-   puts "Please enter a hobby"
-   hobby = gets.chomp.capitalize
-   # country of your birth
-   puts "Please enter the country of your birth"
-   country = gets.chomp.downcase
-   # height
-   puts "Please enter your height"
-   height = gets.chomp.to_s
-   # while the name is not empty: repeat
+   students = [] # Empty array
+   name = gets.chomp.capitalize # get the first name
    while !name.empty? do
-     #add the student hash to the array
-     students << {name: name, cohort: cohort, hobby: hobby, country: country, height: height}
-     puts "Now we have #{students.count} students"
-     # get another name from the user
-     name = gets.chomp.capitalize
+     puts "Please enter the month of the cohort" #cohort
      cohort = gets.chomp.capitalize
-     hobby = gets.chomp.downcase
-     country = gets.chomp.capitalize
-     height = gets.chomp.to_s
+     #add the student hash to the array
+     students << {name: name, cohort: cohort}
+     if (students.count > 1)
+       puts "Now we have #{students.count} students"
+     else
+       puts "Now we have #{students.count} student"
+     end
+     puts "Please enter the details of another student. Hit the return key twice to finish."
+     name = gets.chomp.capitalize # get another name from the user
    end
-   # return the array of students
-     students
+     return students # return the array of students
    end
    # Header
- def print_header
+def print_header
      puts "The students of Villains Academy".center(75)
      puts "-------------".center(75)
  end
 
     # Print hashes
- def print (students)
+def print (students)
    i = 0
    lineWidth = 40
    while students.count > i
-     puts "#{i+1}: #{students[i][:name]} (#{students[i][:cohort]} cohort) - Enjoys: #{students[i][:hobby]} - Born: #{students[i][:country]} - #{students[i][:height]} tall".center(25)
+     puts "#{i+1}: #{students[i][:name]} (#{students[i][:cohort]} cohort)".ljust(25)
      i += 1
    end
- end
+end
     # Print footer & .count
- def print_footer(names)
+def print_footer(names)
    puts
-   puts "Overall, we have #{names.count} great students".center(75)
- end
+   if (names.count > 1)
+     puts "Overall, we have #{names.count} great students".center(75)
+   else
+     puts "Overall, we have #{names.count} great student".center(75)
+   end
+end
+
 
 # Calling methods
 students = input_students
