6b6ddc102d88c38d32d01d98e612dfe31605892b
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-08-21 01:59:21 +0100

    10. Refactoring and 11. Saving data to file

--- directory.rb
+++ directory.rb
@@ -1,27 +1,6 @@
+@students = []
+
 # Methods
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else puts "I don't know what you meant, try again"
-    end
-  end
-end
 
 def input_students
    puts "Please enter the full name of the student"
@@ -42,17 +21,65 @@ def input_students
      age = gets.chomp.to_s
 
     #add the student hash to the array
-     students << {name: name, cohort: cohort, country: country, age: age}
-     if (students.count > 1)
-       puts "Now we have #{students.count} students"
+     @students << {name: name, cohort: cohort, country: country, age: age}
+     if (@students.count > 1)
+       puts "Now we have #{@students.count} students"
      else
-       puts "Now we have #{students.count} student"
+       puts "Now we have #{@students.count} student"
      end
      puts "Please enter the details of another student. Hit the return key twice to finish."
      name = gets.chomp.capitalize # get another name from the user
    end
      return students # return the array of students
-   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:country], student[:age]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process (selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+end
+
    # Header
  def print_header
      puts "The students of Villains Academy".center(75)
@@ -60,24 +87,24 @@ def input_students
  end
 
     # Print hashes
-def print (students)
+def print_students_list
    i = 0
    lineWidth = 40
-   if students.count == 0
+   if @students.count == 0
     return nil
   end
-   while students.count > i
-     puts "#{i+1}: #{students[i][:name]} (#{students[i][:cohort]} cohort). Born: #{students[i][:country]} - Age: #{students[i][:age]}".ljust(25)
+   while @students.count > i
+     puts "#{i+1}: #{@students[i][:name]} (#{@students[i][:cohort]} cohort). Born: #{@students[i][:country]} - Age: #{@students[i][:age]}".ljust(25)
      i += 1
    end
 end
     # Print footer & .count
-def print_footer(names)
+def print_footer
    puts
-   if (names.count > 1)
-     puts "Overall, we have #{names.count} great students".center(75)
+   if (@students.count > 1)
+     puts "Overall, we have #{@students.count} great students".center(75)
    else
-     puts "Overall, we have #{names.count} great student".center(75)
+     puts "Overall, we have #{@students.count} great student".center(75)
    end
 end
 
